b6518659809268efb4132a92e966c65f542f7d80
Julio Adrian Peña Romero <j.adrian.1690@gmail.com>
2021-03-14 18:48:50 -0500

    build: transform to stateless components

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Book from './Book';
 import PropTypes from 'prop-types';
 
-class BooksGrid extends Component {
-
-    render() {
-        const { books, onUpdateBook, onAddBook, booksShelf } = this.props
-        if (!books || books.length === 0) return null;
-        return (
-            <ol className="books-grid">
-                {
-                    books.map(book => (
-                        <Book
-                            key={book.id}
-                            book={book}
-                            onUpdateBook={onUpdateBook}
-                            onAddBook={onAddBook}
-                            booksShelf={booksShelf}
-                        />
-                    ))
-                }
-            </ol>
-        )
-    }
+const BooksGrid = (props) => {
+    const { books, onUpdateBook, onAddBook, booksShelf } = props
+    if (!books || books.length === 0) return null;
+    return (
+        <ol className="books-grid">
+            {
+                books.map(book => (
+                    <Book
+                        key={book.id}
+                        book={book}
+                        onUpdateBook={onUpdateBook}
+                        onAddBook={onAddBook}
+                        booksShelf={booksShelf}
+                    />
+                ))
+            }
+        </ol>
+    )
 }
 
 BooksGrid.propTypes = {
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,55 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BooksGrid from './BooksGrid';
 import PropTypes from 'prop-types';
 
-class ListBooks extends Component {
-    render() {
-        const { books, history, onUpdateBook } = this.props;
+const ListBooks = (props) => {
 
-        const currentlyReadingBooks = books.filter(book => book.shelf === "currentlyReading")
-        const wantToReadBooks = books.filter(book => book.shelf === "wantToRead")
-        const readBooks = books.filter(book => book.shelf === "read")
+    const { books, history, onUpdateBook } = props;
 
-        return <div className="list-books">
-            <div className="list-books-title">
-                <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-                <div>
-                    <div className="bookshelf">
-                        <h2 className="bookshelf-title">Currently Reading</h2>
-                        <div className="bookshelf-books">
-                            <BooksGrid
-                                books={currentlyReadingBooks}
-                                onUpdateBook={onUpdateBook}
-                            />
-                        </div>
+    const currentlyReadingBooks = books.filter(book => book.shelf === "currentlyReading")
+    const wantToReadBooks = books.filter(book => book.shelf === "wantToRead")
+    const readBooks = books.filter(book => book.shelf === "read")
+
+    return <div className="list-books">
+        <div className="list-books-title">
+            <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+            <div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <div className="bookshelf-books">
+                        <BooksGrid
+                            books={currentlyReadingBooks}
+                            onUpdateBook={onUpdateBook}
+                        />
                     </div>
-                    <div className="bookshelf">
-                        <h2 className="bookshelf-title">Want to Read</h2>
-                        <div className="bookshelf-books">
-                            <BooksGrid
-                                books={wantToReadBooks}
-                                onUpdateBook={onUpdateBook}
-                            />
-                        </div>
+                </div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Want to Read</h2>
+                    <div className="bookshelf-books">
+                        <BooksGrid
+                            books={wantToReadBooks}
+                            onUpdateBook={onUpdateBook}
+                        />
                     </div>
-                    <div className="bookshelf">
-                        <h2 className="bookshelf-title">Read</h2>
-                        <div className="bookshelf-books">
-                            <BooksGrid
-                                books={readBooks}
-                                onUpdateBook={onUpdateBook}
-                            />
-                        </div>
+                </div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Read</h2>
+                    <div className="bookshelf-books">
+                        <BooksGrid
+                            books={readBooks}
+                            onUpdateBook={onUpdateBook}
+                        />
                     </div>
                 </div>
             </div>
-            <div className="open-search">
-                <button onClick={() => history.push('/search')}>Add a book</button>
-            </div>
         </div>
-    }
+        <div className="open-search">
+            <button onClick={() => history.push('/search')}>Add a book</button>
+        </div>
+    </div>
 
 }
 
